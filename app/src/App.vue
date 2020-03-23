c6381a78674eeede49127ae712a39d739c993dad
enes <eneskarali96@gmail.com>
2020-03-23 16:29:50 +0300

    vue initialize to project

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/login_icon.png" width="200" height="200">
+    <loginPage/>
+  </div>
+</template>
+
+<script>
+import loginPage from './components/loginPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    loginPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/loginPage.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="page">
+    <div class="login">  
+        <h1>Kullanıcı Bilgileri</h1>      
+        <ul>
+           <li><p>Kullanıcı Adı:</p><input type="text"/></li>
+           <li><p>Şifre:</p><input type="password"/></li>
+        </ul>          
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+p{
+    float: left;   
+    font-size: 22px;
+    margin:2px;
+    color: black;
+    font: normal;
+}
+h1 {
+    margin-top:10px;
+    margin-bottom: 15px;
+    color: black;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    border-style: solid;
+    border-color: #4caf50;
+    border-block-width: 20px;
+    border-inline-width: 15px;
+    border-radius: 10px;
+    background-color: #4caf50;
+
+}
+li {
+    margin: 15px 10px;
+}
+input{
+    height: 35px;
+    width: 98%;
+    font-size: 20px;
+}
+.login{
+    margin-top: 70px;
+    text-align: center;
+    width: 550px;
+    display: inline-block;
+    
+}
+
+.page{
+    text-align: center;
+}
+@media(max-width: 800px){
+    input{
+        width: 320px;
+    }
+
+}
+
+
+</style>
