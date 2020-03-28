d839fbc04b5b4f7536da815fc837849ec98a29f3
enes <eneskarali96@gmail.com>
2020-03-28 16:22:42 +0300

    scroll view added to dashboard page

--- app/src/components/dashboard.vue
+++ app/src/components/dashboard.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="page">
-    <headerComp></headerComp>
+    <headerComp class="header"></headerComp>
+    <div class="content">
+      <Scroll-view>
+        <template slot-scope="visibility">
+          <img v-for="i in items" :src="i.url" :key="i.id" :visibile="visibility" />
+        </template>
+      </Scroll-view>
+    </div>
     <div class="button">
-      <img class="add_icon" src="@/assets/add_icon.png" alt="add icon" width="30px" height="30px" />
+      <img class="add_icon" src="@/assets/add_icon.png" alt="add icon" width="34px" height="34px" />
       <p class="add_text">
         <b>ADD</b>
       </p>
@@ -12,6 +19,8 @@
 
 <script>
 import headerComp from "./headerComp.vue";
+import axios from "axios";
+
 export default {
   name: "dashboard",
   props: {
@@ -19,6 +28,55 @@ export default {
   },
   components: {
     headerComp
+  },
+  data() {
+    return {
+      page: 1,
+      items: [],
+      loading: true
+    };
+  },
+  watch: {
+    page: {
+      immediate: true,
+      handler: function() {
+        this.fetchMore();
+      }
+    }
+  },
+  methods: {
+    fetchMore() {
+      this.loading = true;
+      axios
+        .get(`https://jsonplaceholder.typicode.com/albums/${this.page}/photos`)
+        .then(({ data }) => {
+          this.loading = true;
+          this.items = this.items.concat(data.slice(1, 6));
+        })
+        .catch(e => {
+          this.loading = false;
+          console.log(e);
+        });
+    },
+    scroll() {
+      window.onscroll = () => {
+        let bottomOfWindow =
+          Math.max(
+            window.pageYOffset,
+            document.documentElement.scrollTop,
+            document.body.scrollTop
+          ) +
+            window.innerHeight ===
+          document.documentElement.offsetHeight;
+
+        if (bottomOfWindow) {
+          this.fetchMore();
+        }
+      };
+    }
+  },
+  mounted() {
+    this.scroll();
   }
 };
 </script>
@@ -26,10 +84,10 @@ export default {
 <style scoped>
 .button {
   color: #ffffff !important;
-  background: #000000;
+  background: #4caf50;
   margin-bottom: 0;
   padding: 15px;
-  border: 4px solid #494949 !important;
+  border: 4px solid #000000 !important;
   display: inline-block;
   transition: all 0.3s ease 0s;
   cursor: pointer;
@@ -44,7 +102,8 @@ export default {
 }
 
 .button:hover {
-  color: #4caf50 !important;
+  color: #000000 !important;
+  border: 4px solid #a0c2a1 !important;
   border-radius: 50px;
   transition: all 0.5s ease 0s;
 }
@@ -56,9 +115,24 @@ export default {
 }
 
 .add_text {
-  margin-top: 5px;
+  margin-top: 7px;
   padding-right: 5px;
   font-size: 18px;
   display: inline-block;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.header {
+  position: fixed;
+  top: 0px;
+  width: 100%;
+  height: 0;
+}
+.content {
+  margin-top: 80px;
 }
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import scroolView from 'vue-scrollview'
+
+Vue.use(scroolView)
+
 
 Vue.config.productionTip = false
 
